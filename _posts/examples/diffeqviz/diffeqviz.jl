f1210cce4fd082723ea08f27dfe3e7337bac5417
Stefan de Lange <langestefan@msn.com>
2025-02-16 16:33:47 +0000

    Plot diffeq in a browser

--- _posts/examples/diffeqviz/diffeqviz.jl
+++ _posts/examples/diffeqviz/diffeqviz.jl
@@ -39,34 +39,54 @@ open("_posts/examples/diffeqviz/diffeq.html", "w") do io
     # create sub session
     sub = Session(session)
 
-    app = App() do
-        # Create the slider
-        scale_slider = Slider(1:3)
+    function lorenz(du, u, p, t)
+        x, y, z = u
+        sigma, rho, beta = p
+        du[1] = sigma * (y - x)
+        du[2] = x * (rho - z) - y
+        du[3] = x * y - beta * z
+    end
 
-        # Define an observable that computes our data from the slider value.
-        states = lift(a -> (
-            x = sin.(a .* range(0, stop=10, length=100)),
-            y = cos.(a .* range(0, stop=10, length=100)),
-            z = range(0, stop=10, length=100)
-        ), scale_slider.value)
+    t_begin = 0.0
+    t_end = 100.0
+    tspan = (t_begin, t_end)
+    u_begin = [1.0, 0.0, 0.0]
+
+    app = App() do
+
+        # create the slider
+        slider = Slider(1:6)
+
+        # map slider values to surface states
+        states = map(slider.value) do a_val
+            p = [10.0, 28.0, a_val]
+            prob = ODEProblem(lorenz, u_begin, tspan, p)
+            sol = solve(prob, Tsit5(), reltol = 1e-8, abstol = 1e-8, saveat=0.1)
+
+            x = [u[1] for u in sol.u]
+            y = [u[2] for u in sol.u]
+            z = [u[3] for u in sol.u]
+
+            return (x=x, y=y, z=z)
+        end   
 
         # Now “lift” each field out of the NamedTuple.
         x_obs = lift(s -> s.x, states)
         y_obs = lift(s -> s.y, states)
         z_obs = lift(s -> s.z, states)
+        
 
-        # Use these observables directly in the plot.
-        fig, ax, sc = meshscatter(x_obs, y_obs, z_obs; markersize = 1)
-
-        # Return the plot and slider.
-        return Bonito.record_states(sub, DOM.div(fig, scale_slider))
-    end
-
+        # create the figure
+        fig, = plot(x_obs, y_obs, z_obs; 
+            markersize=3.0, color=:blue
+        ) 
+    
+        # Return the plot and the slider
+        return Bonito.record_states(sub, DOM.div(fig, slider))
+    end;
+    
     as_html(io, sub, app)
 end
-
-
-
 
 # # plot 3 - differential equation
 # open("_posts/examples/diffeqviz/diffeq.html", "w") do io
